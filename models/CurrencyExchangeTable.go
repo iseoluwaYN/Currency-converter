f0a40dc473f4b3e1942878410f0dfd92d92244c7
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-02-06 21:40:03 +0100

    exchange table

--- models/CurrencyExchangeTable.go
+++ models/CurrencyExchangeTable.go
@@ -1,5 +1,15 @@
-package main
+package models
 
-func main() {
-	$END$
+type ExchangeTable struct {
+	CurrencyTable map[string]map[string]float64
+}
+
+func CreateExchangeTable() *ExchangeTable {
+	exchangeTable := new(ExchangeTable)
+	exchangeTable.CurrencyTable = make(map[string]map[string]float64)
+	exchangeTable.CurrencyTable["NGN"] = map[string]float64{"GHG": 0.017, "KSH": 0.27}
+	exchangeTable.CurrencyTable["GHG"] = map[string]float64{"NGN": 60.27, "KSH": 16.46}
+	exchangeTable.CurrencyTable["KSH"] = map[string]float64{"NGN": 3.66, "GHS": 0.061}
+
+	return exchangeTable
 }
